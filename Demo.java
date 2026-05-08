75760e286e58ab3ed02e3bc753a95a27286f46cd
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-08 18:39:09 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+// This is a simple Java program that prints "Hello, GitHub!" to the console.
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+    }
+}
+
+// To run this program, save it in a file named "Demo.java",
+// compile it using the command "javac Demo.java", and then execute it using the command "java Demo".
+// You should see the output "Hello, GitHub!" in the console.
