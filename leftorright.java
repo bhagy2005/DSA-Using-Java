4f1590102c1182898eb4d1337f123e86f15151c4
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-08 19:54:39 +0530

    Add files via upload

---
+++ leftorright.java
@@ -0,0 +1,20 @@
+// This is a simple Java program that determines if a person is a boy or a girl based on a string variable and prints a message accordingly.
+public class leftorright {
+    public static void main(String[] arge) {
+        String person1 = "Girl";
+
+        if (person1 == "Girl")
+        {
+            System.out.println("Go to the left side.");
+        }
+        else
+        {
+            System.out.println("Go to the right side.");
+        }
+    }
+}
+// To run this program, save it in a file named "boyorgirl.java",
+// compile it using the command "javac boyorgirl.java", and then execute it using the command "java boyorgirl".
+
+// You should see the output "Go to the left side." in the console since the variable "person1" is assigned the value "Girl". 
+// If you change the value of "person1" to "Boy", the output will be "Go to the right side.".
