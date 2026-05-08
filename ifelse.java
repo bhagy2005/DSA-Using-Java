5252797965b3c4f60529f7389b16e23d66cc39e2
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-08 19:28:53 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int number = 17;
+
+        if (number > 18) {
+            System.out.println("Eligible to vote.");
+        } else if (number < 18) {
+            System.out.println("Not eligible to vote.");
+        } else {
+            System.out.println("Just eligible to vote.");
+        }
+    }
+}
