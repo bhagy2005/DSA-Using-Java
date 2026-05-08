9ebe0d9aa3db6306afa26df19eaae3c1d107aa30
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-08 19:05:52 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,15 @@
+// This is a simple Java program that demonstrates the use of variables.
+public class variable {
+    public static void main(String[] args) {
+        int a = 10; // This is an integer variable
+        double b = 20.5; // This is a double variable
+        String student = "Bhagya"; // This is a string variable
+
+        System.out.println("Integer variable a: " + a);
+        System.out.println("Double variable b: " + b);
+        System.out.println("String variable student: " + student);
+    }
+}
+// To run this program, save it in a file named "variable.java",
+// compile it using the command "javac variable.java", and then execute it using the command "java variable".
+// You should see the output of the integer, double, and string variables in the console
