8b3ded36e6b8c15baec07cd1ffc3054866ab76b9
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-08 19:15:26 +0530

    Update variable.java

--- variable.java
+++ variable.java
@@ -1,9 +1,9 @@
-// This is a simple Java program that demonstrates the use of variables.
+// This is a simple Java program that demonstrates the use of variables and data types.
 public class variable {
     public static void main(String[] args) {
-        int a = 10; // This is an integer variable
-        double b = 20.5; // This is a double variable
-        String student = "Bhagya"; // This is a string variable
+        int a = 10; // This is a integer datatype and "a" is variable
+        double b = 20.5; //// This is a double datatype and "b" is variable
+        String student = "Bhagya"; //// This is a string datatype and "student" is variable
 
         System.out.println("Integer variable a: " + a);
         System.out.println("Double variable b: " + b);
