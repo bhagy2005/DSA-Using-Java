8ac1ec4c1f7937846eae4ccfeb4dedfd44b2b92c
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-09 12:55:15 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+// This program demonstrates the use of arrays in Java. It initializes an array of integers and prints the value at a specific index.
+// integer array initialization and access
+public class array {
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4,5};
+        System.out.println(num[2]);
+    }
+}
+// Output: 3
+
+// String array initialization and access
+ 
+/*public class array {
+    public static void main(String[] args) {
+        String names[] = {"Python", "Java", "JavaScript", "C++"};
+        System.out.println(names[1]);
+    }
+}*/
+
+// Output: Java
