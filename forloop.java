deb00a9dea56beaf9b004cdbc9b40c50c860c17c
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-09 16:43:17 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,18 @@
+// This program demonstrates the use of a for loop in Java. It initializes an array of integers and uses a for loop to print each element of the array.
+public class forloop {
+    public static void main(String[] args) {
+        // for loop to print numbers from 1 to 5
+        int num[] = {1, 2, 3, 4, 5};
+        for(int i=0; i<5; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
+
+/*Output:
+1
+2
+3
+4
+5
+*/
