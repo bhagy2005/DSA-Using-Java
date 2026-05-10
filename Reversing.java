9c19431cc1ea191ab6951f8fd0beb7673b60db64
BHAGYA SRILAKSHMI DAMPANABOYINA <bhagyasrilakshmidampanaboyina@gmail.com>
2026-05-10 15:19:51 +0530

    Add files via upload

---
+++ Reversing.java
@@ -0,0 +1,18 @@
+// Reversing a string using charAt() method, is used to reverse order of characters in a string. 
+// By using a loop to iterate through the string from the last index to the first index, we can print the characters in reverse order.    
+// example: BHAGYA -- AYGAHB
+public class Reversing {
+    public static void main(String[] args) {
+        String name = "BHAGYA";
+        for(int i=5; i>=0; i--){
+            System.out.println(name.charAt(i));
+            }
+    }
+}
+// Output:
+// A
+// Y        
+// G
+// A
+// H
+// B
